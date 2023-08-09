352bd3ef3e32eb7ba7cbbe64ccbbeeb75f5d750a
Vayom PC <korefan639@gmail.com>
2023-08-09 23:18:40 +0300

    Add some comments

--- characters/characters.py
+++ characters/characters.py
@@ -17,12 +17,17 @@ class Character:
         1) name - имя персонажа
         2) hp - Health Point (очки здоровья)
         3) attack - количество урона, наносимое персонажем
+        4) speed - скорость (нужна для определения стороны для первого удара)
+        5) crit_chance - шанс нанести крит. урон (в процентах от 0 до 100)
+        6) crit_damage - урон, наносимый при срабатывании крита (в процентах от базовой атаки)
+        7) avoid_chance - шанс парировать атаку соперника (в процентах)
     '''
 
     def __init__(self, name, hp, attack, speed, crit_chance, crit_damage, avoid_chance):
         self.name = name
         self.hp = hp
         self.attack = attack
+        self.speed = speed
         self.crit_chance = crit_chance
         self.crit_damage = crit_damage
         self.avoid_chance = avoid_chance
--- items/static_items.py
+++ items/static_items.py
@@ -1,4 +1,17 @@
 class StaticItem:
+    '''
+        Описание
+            Класс статических предметов, которые игрок может надеть
+        Параметры:
+            1) title - название предмета
+            2) hp - Health Point (очки здоровья)
+            3) attack - количество урона, наносимое персонажем
+            4) speed - скорость (нужна для определения стороны для первого удара)
+            5) crit_chance - шанс нанести крит. урон (в процентах от 0 до 100)
+            6) crit_damage - урон, наносимый при срабатывании крита (в процентах от базовой атаки)
+            7) avoid_chance - шанс парировать атаку соперника (в процентах)
+        '''
+
     def __init__(self, title, hp, attack, speed, crit_chance, crit_damage, avoid_chance):
         self.used = False
         self.title = title
@@ -8,4 +21,3 @@ class StaticItem:
         self.crit_chance = crit_chance
         self.crit_damage = crit_damage
         self.avoid_chance = avoid_chance
-
